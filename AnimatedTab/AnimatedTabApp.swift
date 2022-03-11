e3a45f87f440731e76066df4ed6f9abf50f9465e
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-11 09:32:15 +0530

    Initial Commit

---
+++ AnimatedTab/AnimatedTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTabApp.swift
+//  AnimatedTab
+//
+//  Created by shehan karunarathna on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedTab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedTab
+//
+//  Created by shehan karunarathna on 2022-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
